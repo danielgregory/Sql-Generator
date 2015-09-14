4c1110d96ad7a0afeb5abc903c71ec9104717b55
danielgregory <dg334@outlook.com>
2015-09-14 21:09:42 +0100

    test commit

---
+++ data/data.py
@@ -0,0 +1,36 @@
+__author__ = 'Daniel Gregory'
+from distributions.gaussian import gaussian_generator
+
+
+# returns data of the form # [{"NAME": "Donald", "AGE": 54}, ...]
+# columns looks like: {"name": str, "age": int}
+# n corresponds to the number of rows of data to return
+def generate_column_value_pairs(columns, n):
+    # generators the distributions
+    gs = generate_names()
+    gg = gaussian_generator(5, 5)
+
+    # return result
+    data_list = []
+    for _ in range(n):
+        for k, v in columns.items():
+            data_dict = {}
+            if v == str:
+                data_dict[k] = next(gs)
+            elif v == int:
+                data_dict[k] = round(next(gg))
+            else:
+                raise Exception("Type not found")
+            data_list.append(data_dict)
+    return data_list
+
+
+def generate_names(filename):
+    with open(filename) as f:
+        lines = f.readlines()
+        return (line for line in lines)
+
+
+
+
+
